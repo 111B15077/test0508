4a590e3926c83cb3111cf95ae1f050e01cd28d48
yum <111B15077@mailst.cjcu.edu.tw>
2024-05-08 15:45:22 +0800

    5625463541

--- app/src/main/java/com/example/test0508/stuDataact.java
+++ app/src/main/java/com/example/test0508/stuDataact.java
@@ -1,6 +1,9 @@
 package com.example.test0508;
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import java.util.List;
 
 import androidx.annotation.NonNull;
@@ -9,26 +12,35 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class stuDataact extends RecyclerView.Adapter<stuDataact.ViewHolder> {
 
+    private static TextView tvname;
+    private static TextView tvheight;
     private List<stuData> listData;
 
     public  stuDataact (List<stuData> listData){
         this.listData = listData;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            tvname = itemView.findViewById(R.id.tvname);
+            tvheight = itemView.findViewById(R.id.tvheight);
         }
     }
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        Context context = parent.getContext();
+        View  view = View.inflate(context, R.layout.activity_my_data_view, null);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        stuData stuData = listData.get(position);
+        holder.tvname.setText(stuData.getName());
+        holder.tvheight.setText(stuData.getHeight());
     }
 
     @Override
