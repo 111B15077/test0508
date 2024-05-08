409789c00856714906fa3a5711c9bec250b808ed
yum <111B15077@mailst.cjcu.edu.tw>
2024-05-08 14:15:24 +0800

    212321

---
+++ app/stuData.java
@@ -0,0 +1,36 @@
+public class stuData {
+    private String id;
+    private String name;
+    private String height;
+
+
+    public stuData(String id, String name, String height) {
+        this.id = id;
+        this.name = name;
+        this.height = height;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+}
